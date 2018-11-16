91447dd4e12312c9f594a93295db47b7db11d3a9
Hong Le <Hongle050@gmail.com>
2018-11-16 16:56:08 +0000

    Hang man style game
    Game called Apple Pie. The game is based off Hang man. I used the App development with sift for guidance. Purpose to understand how to use constraints and learn and understand more about UIKit and the foundation of iOS Development

--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -10,11 +10,81 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var treeImageView: UIImageView!
+    @IBOutlet weak var correctScoredLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet var letterButtons: [UIButton]!
+    var listOfWords = ["hong","cong","kelly","michael"]
+    let incorrectMovesAllowed = 7
+    var totalWins = 0{
+        didSet {
+            newRound()
+        }
+    }
+    var totalLosses = 0{
+        didSet {
+            newRound()
+        }
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        newRound()
+    }
+    
+    var currentGame: Game!
+    
+    
+    func newRound() {
+        if !listOfWords.isEmpty {
+            let newWord = listOfWords.removeFirst()
+            currentGame = Game(word: newWord,
+                               incorrectMovesRemaining: incorrectMovesAllowed,
+                               guessedLetters: [])
+            enableLetterButtons(true)
+            updateUI()
+        } else {
+            enableLetterButtons(false)
+        }
+
+    }
+    
+    func updateUI(){
+        var letters = [String]()
+        for letter in currentGame.formattedWord.characters {
+            letters.append(String(letter))
+        }
+        let wordWithSpacing = letters.joined(separator: " ")
+        correctScoredLabel.text = wordWithSpacing
+        scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses), Guesses: \(currentGame.incorrectMovesRemaining)"
+    }
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        sender.isEnabled = false
+        let letterString = sender.title(for: .normal)!
+        let letter = Character(letterString.lowercased())
+        currentGame.playerGuessed(letter: letter)
+        updateUI()
+
+    }
+    func updateGameState() {
+          if currentGame.incorrectMovesRemaining == 0 {
+            totalLosses += 1
+        } else if currentGame.word == currentGame.formattedWord {
+            totalWins += 1
+        } else {
+            updateUI()
+        }
+    }
+    func enableLetterButtons(_ enable: Bool) {
+        for button in letterButtons {
+            button.isEnabled = enable
+        }
     }
 
+    
 
 }
 
